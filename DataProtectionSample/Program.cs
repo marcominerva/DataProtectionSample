2ddabc912a4b977cfe53520378df5d47c15d5264
Marco Minerva <marco.minerva@gmail.com>
2024-04-03 12:21:23 +0200

    Update libraries

--- DataProtectionSample/Program.cs
+++ DataProtectionSample/Program.cs
@@ -32,6 +32,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
+app.UseExceptionHandler();
+app.UseStatusCodePages();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
